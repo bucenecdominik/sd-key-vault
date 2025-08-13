b9e720c0dca11c2e7d953afc485434faa3b5ea0e
bucenecdominik <61663143+bucenecdominik@users.noreply.github.com>
2025-08-13 16:06:02 +0200

    feat: add basic routing structure

---
+++ src/pages/FoldersPage.tsx
@@ -0,0 +1,10 @@
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+
+export default function FoldersPage() {
+  return (
+    <Box p={3}>
+      <Typography variant="h4">Seznam složek</Typography>
+    </Box>
+  )
+}
