dfb3ed1f071e2b2210c675953232031177b214c2
bucenecdominik <61663143+bucenecdominik@users.noreply.github.com>
2025-08-13 15:11:53 +0200

    feat: use domain types on main page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,34 @@ import Sidebar from './components/Sidebar'
 import SearchBar from './components/SearchBar'
 import PasswordGrid from './components/PasswordGrid'
 import PasswordDetail from './components/PasswordDetail'
+import { Password } from './types'
+
+const passwords: Password[] = [
+  {
+    Id: 1,
+    FolderId: 1,
+    Name: 'Heslo 1',
+    Description: 'Popis 1',
+    PasswordId: 1,
+    UserName: 'user1',
+    CreatedBy: 1,
+    CreatedAt: new Date(),
+    UpdatedBy: 1,
+    UpdatedAt: new Date(),
+  },
+  {
+    Id: 2,
+    FolderId: 1,
+    Name: 'Heslo 2',
+    Description: 'Popis 2',
+    PasswordId: 2,
+    UserName: 'user2',
+    CreatedBy: 1,
+    CreatedAt: new Date(),
+    UpdatedBy: 1,
+    UpdatedAt: new Date(),
+  },
+]
 
 export default function App() {
   return (
@@ -10,7 +38,7 @@ export default function App() {
       <Sidebar />
       <Box flexGrow={1} display="flex" flexDirection="column">
         <SearchBar />
-        <PasswordGrid />
+        <PasswordGrid passwords={passwords} />
       </Box>
       <PasswordDetail />
     </Box>
